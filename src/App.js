126b9926fcfe11bcd9ecf993c7dcabccca1c5cfb
Bhupendra <bhupendra.scripts@gmail.com>
2025-03-20 18:49:07 +0530

    added loader in start

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import AvailableCourses from "./components/AvailableCourses";
+import Loader from "./components/Loader"; // Import Loader component
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1500); // Show loader for 2 seconds
+  }, []);
+
   return (
     <Router>
-      <div className="App">
-        <Navbar />  {/* Navbar stays visible on all pages */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/courses" element={<AvailableCourses />} />
-        </Routes>
-      </div>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="App">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/courses" element={<AvailableCourses />} />
+          </Routes>
+        </div>
+      )}
     </Router>
   );
 }
---
+++ src/components/Loader.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Icons.css"
+
+const Loader = () => {
+  return (
+    <div className="w-screen h-[90vh] sm:h-screen grid place-items-center">
+        <div className="loader14"></div>
+    </div>
+  );
+};
+
+export default Loader;
